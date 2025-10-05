5f4bbd2a24751ed166f26720358a9b1c02555888
Sampurna Adhikari <sampurnaa19@gmail.com>
2025-10-05 11:32:58 +1100

    copy and paste this command ( npm install firebase) in Terminal VS

---
+++ utils/firbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBYJATJ-a2hVYwz18p3iB4-oVqUWuwpNs",
+  authDomain: "weightmate-79469.firebaseapp.com",
+  projectId: "weightmate-79469",
+  storageBucket: "weightmate-79469.firebasestorage.app",
+  messagingSenderId: "75474028125",
+  appId: "1:75474028125:web:dc3ee29aaf728eb0bb6fb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
